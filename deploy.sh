6549a66275f5214283be69f3e75528df1965dc76
Almasi <aaalmasi@sail.cive.e.uh.edu>
2023-08-05 15:10:53 -0500

    bashh

--- deploy.sh
+++ deploy.sh
@@ -1,8 +1,26 @@
 #!/bin/bash
-echo Enter Username:
-read username
-echo Enter Password:
-read password
-export username=username
-export password=username
+
+# Function to read username and password arguments
+read_arguments() {
+    # Check if both arguments are provided
+    if [ $# -ne 2 ]; then
+        echo "Error: Please provide both username and password arguments"
+        exit 1
+    fi
+    
+    # Assign arguments to variables
+    username=$1
+    password=$2
+    
+    # Print the username and password
+    export username=$username
+    export password=$password
+}
+
+# Wait for five seconds
+sleep 5
+
+# Call the function with the provided arguments
+read_arguments "$@"
+
 docker compose up -d
